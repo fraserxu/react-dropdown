f4fc3b3a44906a9d03757db9d448430c87e0a3e4
Mike Atkins <mike.atkins@lanetix.com>
2015-04-30 15:56:04 -0400

    Set initial state in constructor
    This is the
    [recommended](https://facebook.github.io/react/docs/reusable-components.html#es6-classes)
    way to do it, and it prevents an error which would occur for me wherein, the
    state update would occur before the component had mounted and throw an
    exception.

--- dist/index.js
+++ dist/index.js
@@ -4,6 +4,8 @@ var _interopRequire = function (obj) { return obj && obj.__esModule ? obj["defau
 
 var _createClass = (function () { function defineProperties(target, props) { for (var key in props) { var prop = props[key]; prop.configurable = true; if (prop.value) prop.writable = true; } Object.defineProperties(target, props); } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
+var _get = function get(object, property, receiver) { var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { return get(parent, property, receiver); } } else if ("value" in desc && desc.writable) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } };
+
 var _inherits = function (subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) subClass.__proto__ = superClass; };
 
 var _classCallCheck = function (instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } };
@@ -13,11 +15,12 @@ var React = _interopRequire(require("react"));
 var classNames = _interopRequire(require("classnames"));
 
 var Dropdown = (function (_React$Component) {
-  function Dropdown() {
+  function Dropdown(props) {
     _classCallCheck(this, Dropdown);
 
+    _get(Object.getPrototypeOf(Dropdown.prototype), "constructor", this).call(this, props);
     this.state = {
-      selected: undefined,
+      selected: props.value || { label: "Select...", value: "" },
       isOpen: false
     };
   }
@@ -25,13 +28,6 @@ var Dropdown = (function (_React$Component) {
   _inherits(Dropdown, _React$Component);
 
   _createClass(Dropdown, {
-    componentWillMount: {
-      value: function componentWillMount() {
-        this.setState({
-          selected: this.props.value || { label: "Select...", value: "" }
-        });
-      }
-    },
     componentWillReceiveProps: {
       value: function componentWillReceiveProps(newProps) {
         if (newProps.value && newProps.value !== this.state.selected) {
