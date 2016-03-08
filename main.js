bb176fd0c116f5e7382d3e5f309516dd6178442a
fraserxu <xvfeng123@gmail.com>
2016-03-08 16:33:49 +1100

    Updates

---
+++ main.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+import FlatArrayExample from './flatArrayExample'
+import ObjectArrayExample from './objectArrayExample'
+
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <header>
+          <h2><a href='https://github.com/fraserxu/react-dropdown'>React Dropdown</a></h2>
+        </header>
+        <section className='description'>
+          <p>
+            Simple Dropdown component for React, inspired by <a href='https://github.com/JedWatson/react-select'>react-select</a>
+          </p>
+          <div className='code'>
+            <pre>
+              { "$ npm install react-dropdown --save" }
+            </pre>
+          </div>
+        </section>
+
+        <section>
+          <h3>Examples: </h3>
+          <h4>Usage: </h4>
+          <div className='code'>
+            <pre>
+              {`
+<Dropdown options={options} onChange={this._onSelect} value={defaultOption} placeholder="Select an option" />
+              `}
+            </pre>
+          </div>
+        </section>
+
+        <FlatArrayExample />
+        <ObjectArrayExample />
+
+        <section>
+          <h3>License: </h3>
+        </section>
+        <footer>
+          <p>MIT | Build for <a href='https://csviz.org'>CSViz</a> project @<a href='http://wiredcraft.com'>Wiredcraft</a></p>
+        </footer>
+      </div>
+
+    )
+  }
+
+}
+
+ReactDOM.render(<App />, document.querySelector('#app'))
---
+++ objectArrayExample.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react'
+import Dropdown from '../index.js'
+
+class ObjectArrayExample extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      selected: { value: 'two', label: 'Two'}
+    }
+    this._onSelect = this._onSelect.bind(this)
+  }
+
+  _onSelect (option) {
+    console.log('You selected ', option.label)
+    this.setState({selected: option})
+  }
+
+  render () {
+    const options = [
+      { value: 'one', label: 'One' },
+      { value: 'two', label: 'Two' },
+      {
+       type: 'group', name: 'group1', items: [
+         { value: 'three', label: 'Three' },
+         { value: 'four', label: 'Four' }
+       ]
+      },
+      {
+       type: 'group', name: 'group2', items: [
+         { value: 'five', label: 'Five' },
+         { value: 'six', label: 'Six' }
+       ]
+      }
+    ]
+
+    const defaultOption = this.state.selected
+    const placeHolderValue = typeof this.state.selected === 'string' ? this.state.selected : this.state.selected.label
+
+    return (
+      <section>
+        <h4>Object Array Example </h4>
+        <Dropdown options={options} onChange={this._onSelect} value={defaultOption} placeholder="Select an option" />
+        <div className='result'>
+          You selected
+          <strong> {placeHolderValue} </strong>
+        </div>
+        <section>
+          <h3>Options: </h3>
+          <div className='code'>
+            <pre>
+              {`
+const options = [
+{ value: 'one', label: 'One' },
+{ value: 'two', label: 'Two' },
+{
+ type: 'group', name: 'group1', items: [
+   { value: 'three', label: 'Three' },
+   { value: 'four', label: 'Four' }
+ ]
+},
+{
+ type: 'group', name: 'group2', items: [
+   { value: 'five', label: 'Five' },
+   { value: 'six', label: 'Six' }
+ ]
+}
+]
+              `}
+            </pre>
+          </div>
+        </section>
+      </section>
+    )
+  }
+}
+
+export default ObjectArrayExample
