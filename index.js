df22b542093361f9edcd9d438c8b5f99b2bca03e
Alex Beauchemin <alexbeauchemin01@gmail.com>
2016-05-01 22:24:01 -0400

    Add changes to source

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ class Dropdown extends Component {
   componentWillReceiveProps (newProps) {
     if (newProps.value && newProps.value !== this.state.selected) {
       this.setState({selected: newProps.value})
-    } else if (newProps.placeholder) {
+    } else if (!newProps.value && newProps.placeholder) {
       this.setState({selected: { label: newProps.placeholder, value: '' }})
     }
   }
