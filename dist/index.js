eb96736cf08f75060c866a7be68ddee1a6e8b01e
dittonjs <jditton.atomic@gmail.com>
2017-01-24 16:50:55 -0700

    fix issue cause state change in parent component to render the default value instead of the actual value

--- dist/index.js
+++ dist/index.js
@@ -60,7 +60,7 @@ var Dropdown = function (_Component) {
         this.setState({ selected: newProps.value });
       } else if (!newProps.value && newProps.placeholder) {
         this.setState({ selected: { label: newProps.placeholder, value: '' } });
-      } else {
+      } else if (!newProps.value) {
         this.setState({ selected: { label: DEFAULT_PLACEHOLDER_STRING, value: '' } });
       }
     }
