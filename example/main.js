7d1c82a67ac4de51b7ebc481d9c250796c4300f5
dittonjs <jditton.atomic@gmail.com>
2017-01-28 13:48:19 -0700

    change some stuff

--- example/main.js
+++ example/main.js
@@ -8,9 +8,6 @@ class App extends Component {
   render () {
     return (
       <div>
-        <header>
-          <h2><a href='https://github.com/fraserxu/react-dropdown'>React Dropdown</a></h2>
-        </header>
         <section className='description'>
           <p>
             Simple Dropdown component for React, inspired by <a href='https://github.com/JedWatson/react-select'>react-select</a>
