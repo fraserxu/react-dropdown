582aba3b8fbe406b7157d9c79aa57f6903805583
Tomi Harju <harjuto@gmail.com>
2017-12-04 17:19:42 +0200

    added initial guess of typescript typings

---
+++ index.d.ts
@@ -0,0 +1,27 @@
+declare module "react-dropdown" {
+  import * as React from "react";
+  interface Option {
+    label: string;
+    value: string;
+  }
+  interface Group {
+    type: "group";
+    name: string;
+    items: Option[];
+  }
+  interface ReactDropdownProps {
+    options: (Group | Option)[];
+    baseClassName: string;
+    className: string;
+    disabled?: boolean;
+    onChange?: (arg: Option) => void;
+    onFocus?: (arg: boolean) => void;
+    value?: Option;
+    placeholder?: String;
+  }
+
+  class ReactDropdown extends React.Component<ReactDropdownProps> {
+  }
+
+  export default ReactDropdown;
+}
