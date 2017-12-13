3197ac7ef770e69c7c67c62faafde8752c87af50
Fraser Xu <xvfeng123@gmail.com>
2017-12-14 00:10:34 +1100

    it compiles :tada:

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,46 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const React = require("react");
-const src_1 = require("../src");
-const options = [
-    'one', 'two', 'three'
-];
-exports.default = () => React.createElement("div", null,
-    React.createElement("h1", null, "React Dropdown Demo"),
-    React.createElement(src_1.default, { options: options }));
+import React from 'react'
+import Head from 'next/head'
+
+import Dropdown from '../dist'
+
+const options = ['one', 'two', 'three']
+
+class Example extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      selected: options[0]
+    }
+    this.onSelect = this.onSelect.bind(this)
+  }
+
+  onSelect(option) {
+    this.setState({
+      selected: option
+    })
+  }
+
+  render() {
+    const { selected } = this.state
+    return (
+      <div>
+        <Head>
+          <title>React Dropdown</title>
+          <link rel="stylesheet" href="/static/style.css" />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
+        <Dropdown
+          options={options}
+          onChange={this.onSelect}
+          value={selected}
+          placeholder="Select an option"
+        />
+      </div>
+    )
+  }
+}
+
+export default Example
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import * as classNames from 'classnames'
 
-interface Option {
+export interface Option {
   label: string
   value: string
   type?: string
@@ -9,32 +9,33 @@ interface Option {
   items?: Array<Option>
 }
 
-interface IProps {
+export interface DropdownProps {
   className?: string
   baseClassName?: string
   disabled?: boolean
   value?: string | Option
   placeholder?: string
-  options?: Array<Option> | Array<string>
-  onFocus?: (boolean) => void
+  options?: Array<Option | string>
+  onFocus?: (isOpen: boolean) => void
+  onChange?: (data: string | Option) => void
 }
 
-interface IState {
+export interface DropdownState {
   isOpen: boolean
   selected: string | Option
 }
 
 const DEFAULT_PLACEHOLDER_STRING = 'Select...'
 
-class Dropdown extends React.Component<IProps, IState> {
+class Dropdown extends React.Component<DropdownProps, DropdownState> {
   private mounted: boolean
+  private container: HTMLDivElement
 
-  static defaultProps = {
+  static defaultProps: DropdownProps = {
     baseClassName: 'Dropdown',
     options: []
   }
-
-  constructor(props: IProps) {
+  constructor(props: DropdownProps) {
     super(props)
 
     this.state = {
@@ -46,41 +47,114 @@ class Dropdown extends React.Component<IProps, IState> {
     }
 
     this.mounted = true
+    this.getContainer = this.getContainer.bind(this)
     this.handleMouseDown = this.handleMouseDown.bind(this)
   }
 
-  handleMouseDown(event) {
+  componentWillReceiveProps(newProps: DropdownProps) {
+    if (newProps.value && newProps.value !== this.state.selected) {
+      this.setState({ selected: newProps.value })
+    } else if (!newProps.value) {
+      this.setState({
+        selected: {
+          label: newProps.placeholder || DEFAULT_PLACEHOLDER_STRING,
+          value: ''
+        }
+      })
+    }
+  }
+
+  componentDidMount() {
+    document.addEventListener(
+      'click',
+      this.handleDocumentClick.bind(this),
+      false
+    )
+    document.addEventListener(
+      'touchend',
+      this.handleDocumentClick.bind(this),
+      false
+    )
+  }
+
+  componentWillUnmount() {
+    this.mounted = false
+    document.removeEventListener(
+      'click',
+      this.handleDocumentClick.bind(this),
+      false
+    )
+    document.removeEventListener(
+      'touchend',
+      this.handleDocumentClick.bind(this),
+      false
+    )
+  }
+
+  getContainer(ref: HTMLDivElement) {
+    this.container = ref
+  }
+
+  handleMouseDown(
+    event: React.MouseEvent<HTMLElement> | React.TouchEvent<HTMLElement>
+  ) {
     const { isOpen } = this.state
     if (this.props.onFocus && typeof this.props.onFocus === 'function') {
       this.props.onFocus(isOpen)
     }
 
-    if (event.type === 'mousedown' && event.button !== 0) return
+    // if (event.type === 'mousedown' && event.button !== 0) return
     event.stopPropagation()
     event.preventDefault()
 
     if (!this.props.disabled) {
       this.setState({
-        isOpen: !this.state.isOpen
+        isOpen: !isOpen
       })
     }
   }
 
-  renderOption(option) {
+  setValue(value: string, label: string) {
+    const newState = {
+      selected: {
+        value,
+        label
+      },
+      isOpen: false
+    }
+    this.fireChangeEvent(newState)
+    this.setState(newState)
+  }
+
+  fireChangeEvent(newState: DropdownState) {
+    if (newState.selected !== this.state.selected && this.props.onChange) {
+      this.props.onChange(newState.selected)
+    }
+  }
+
+  renderOption(option: Option | string) {
     const { baseClassName } = this.props
     const { selected } = this.state
 
-    const optionClassName = classNames(`${this.props.baseClassName}-option`, {
+    const optionClassName = classNames(`${baseClassName}-option`, {
       'is-selected': option === selected
     })
 
-    const value = option.value || option.label || option
-    const label = option.label || option.value || option
+    let value, label
+    if (typeof option === 'string') {
+      value = option
+      label = option
+    } else {
+      value = option.value || option.label
+      value = option.label || option.value
+    }
 
     return (
       <div
         key={value}
         className={optionClassName}
+        onMouseDown={this.setValue.bind(this, value, label)}
+        onClick={this.setValue.bind(this, value, label)}
       >
         {label}
       </div>
@@ -90,13 +164,18 @@ class Dropdown extends React.Component<IProps, IState> {
   buildMenu() {
     const { options, baseClassName } = this.props
     const optionsElements = options.map(option => {
-      if (option.type === 'group') {
+      if (typeof option === 'object' && option.type === 'group') {
         const groupTitleClassName = `${baseClassName}-title`
-        const groupTitleElement = <div className={groupTitleClassName}>{option.name}</div>
-        const subOptionsElements = option.items.map(item => this.renderOption(item))
+        const groupClassName = `${baseClassName}-group`
+        const groupTitleElement = (
+          <div className={groupTitleClassName}>{option.name}</div>
+        )
+        const subOptionsElements = option.items.map(item =>
+          this.renderOption(item)
+        )
 
         return (
-          <div key={option.name}>
+          <div key={option.name} className={groupClassName}>
             {groupTitleElement}
             {subOptionsElements}
           </div>
@@ -107,17 +186,30 @@ class Dropdown extends React.Component<IProps, IState> {
     })
 
     const noOptionClassName = `${baseClassName}-noresults`
-    const noOptionElement = <div className={noOptionClassName}>No options found</div>
+    const noOptionElement = (
+      <div className={noOptionClassName}>No options found</div>
+    )
     return optionsElements.length ? optionsElements : noOptionElement
+  }
+
+  handleDocumentClick(event: React.MouseEvent<HTMLElement>) {
+    if (this.mounted) {
+      if (!this.container.contains(event.target as Node)) {
+        if (this.state.isOpen) {
+          this.setState({
+            isOpen: false
+          })
+        }
+      }
+    }
   }
 
   render() {
     const { baseClassName, className, disabled } = this.props
     const { selected, isOpen } = this.state
 
-    const placeHolderValue = typeof selected === 'string'
-      ? selected
-      : selected.label
+    const placeHolderValue =
+      typeof selected === 'string' ? selected : selected.label
 
     const disabledClassName = disabled ? 'Dropdown-disabled' : ''
     const dropdownRootClassName = classNames(`${baseClassName}-root`, {
@@ -132,24 +224,21 @@ class Dropdown extends React.Component<IProps, IState> {
     const placeHolderArrowClassName = `${baseClassName}-arrow`
     const menuClassName = `${baseClassName}-menu`
 
-    return <div className={dropdownRootClassName}>
-      <div
-        className={dropdownControlClassName}
-        onMouseDown={this.handleMouseDown}
-        onTouchEnd={this.handleMouseDown}
-      >
-        <div className={placeHolderClassName}>
-          {placeHolderValue}
-          <span className={placeHolderArrowClassName} />
-        </div>
-
-        {isOpen &&
-          <div className={menuClassName}>
-            {this.buildMenu()}
+    return (
+      <div className={dropdownRootClassName} ref={this.getContainer}>
+        <div
+          className={dropdownControlClassName}
+          onMouseDown={this.handleMouseDown}
+          onTouchEnd={this.handleMouseDown}
+        >
+          <div className={placeHolderClassName}>
+            {placeHolderValue}
+            <span className={placeHolderArrowClassName} />
           </div>
-        }
+        </div>
+        {isOpen && <div className={menuClassName}>{this.buildMenu()}</div>}
       </div>
-    </div>
+    )
   }
 }
 
