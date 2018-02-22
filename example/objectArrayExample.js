c7aafa74bc479354f663601a7eccac9f6d578440
Andrea da Fonseca <me@adf.ninja>
2018-02-22 18:32:46 +0100

    add examples for custom classes

--- example/objectArrayExample.js
+++ example/objectArrayExample.js
@@ -16,22 +16,25 @@ class ObjectArrayExample extends Component {
   }
 
   render () {
+    const { toggleClassName, togglePlaholderClassName, toggleMenuClassName, toggleOptionsClassName } = this.state
+
     const options = [
       { value: 'one', label: 'One' },
-      { value: 'two', label: 'Two' },
+      { value: 'two', label: 'Two', className: toggleOptionsClassName && 'my-custom-class' },
       {
-       type: 'group', name: 'group1', items: [
-         { value: 'three', label: 'Three' },
-         { value: 'four', label: 'Four' }
+       type: 'group', name: 'Prenotazione immediata', items: [
+         { value: 'three', label: '11:00', className: toggleOptionsClassName && 'my-custom-class' },
+         { value: 'four', label: '15:00' }
        ]
       },
       {
-       type: 'group', name: 'group2', items: [
-         { value: 'five', label: 'Five' },
-         { value: 'six', label: 'Six' }
+       type: 'group', name: 'Prenotazione su conferma', items: [
+         { value: 'five', label: '10:00' },
+         { value: 'six', label: '16:00' }
        ]
       }
     ]
+
 
     const defaultOption = this.state.selected
     const placeHolderValue = typeof this.state.selected === 'string' ? this.state.selected : this.state.selected.label
@@ -39,7 +42,29 @@ class ObjectArrayExample extends Component {
     return (
       <section>
         <h4>Object Array Example </h4>
-        <Dropdown options={options} onChange={this._onSelect} value={defaultOption} placeholder="Select an option" />
+        <div className="buttons">
+          <button onClick={() => this.setState({ toggleClassName: !toggleClassName })}>
+            Toggle dropdown custom class
+          </button>
+          <button onClick={() => this.setState({ togglePlaholderClassName: !togglePlaholderClassName })}>
+            Toggle placeholder custom class
+          </button>
+          <button onClick={() => this.setState({ toggleMenuClassName: !toggleMenuClassName })}>
+            Toggle menu custom class
+          </button>
+          <button onClick={() => this.setState({ toggleOptionsClassName: !toggleOptionsClassName })}>
+            Toggle options custom class
+          </button>
+        </div>
+        <Dropdown
+          options={options}
+          onChange={this._onSelect}
+          value={defaultOption}
+          placeholder="Select an option"
+          className={ toggleClassName ? 'my-custom-class' : '' }
+          placeholderClassName={ togglePlaholderClassName ? 'my-custom-class' : '' }
+          menuClassName={ toggleMenuClassName ? 'my-custom-class' : '' }
+        />
         <div className='result'>
           You selected
           <strong> {placeHolderValue} </strong>
@@ -51,11 +76,11 @@ class ObjectArrayExample extends Component {
               {`
 const options = [
 { value: 'one', label: 'One' },
-{ value: 'two', label: 'Two' },
+{ value: 'two', label: 'Two'${toggleOptionsClassName ? ", classNames: ['my-custom-class']" : ""} },
 {
  type: 'group', name: 'group1', items: [
    { value: 'three', label: 'Three' },
-   { value: 'four', label: 'Four' }
+   { value: 'four', label: 'Four'${toggleOptionsClassName ? ", classNames: ['my-custom-class']" : ""} }
  ]
 },
 {
