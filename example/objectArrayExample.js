224dab0acfd1b9338d27e8fb2aa86d661c63e63c
Andrea da Fonseca <me@adf.ninja>
2018-02-23 12:07:07 +0100

    some beautification to examples

--- example/objectArrayExample.js
+++ example/objectArrayExample.js
@@ -41,7 +41,7 @@ class ObjectArrayExample extends Component {
 
     return (
       <section>
-        <h4>Object Array Example </h4>
+        <h3>Object Array and Custom ClassNames Example </h3>
         <div className="buttons">
           <button onClick={() => this.setState({ toggleClassName: !toggleClassName })}>
             Toggle dropdown custom class
@@ -70,27 +70,42 @@ class ObjectArrayExample extends Component {
           <strong> {placeHolderValue} </strong>
         </div>
         <section>
-          <h3>Options: </h3>
+          <h4>Options: </h4>
           <div className='code'>
             <pre>
               {`
 const options = [
-{ value: 'one', label: 'One' },
-{ value: 'two', label: 'Two'${toggleOptionsClassName ? ', classNames: \'my-custom-class\'' : ''} },
-{
- type: 'group', name: 'group1', items: [
-   { value: 'three', label: 'Three' },
-   { value: 'four', label: 'Four'${toggleOptionsClassName ? ', classNames: \'my-custom-class\'' : ''} }
- ]
-},
-{
- type: 'group', name: 'group2', items: [
-   { value: 'five', label: 'Five' },
-   { value: 'six', label: 'Six' }
- ]
-}
+  { value: 'one', label: 'One' },
+  { value: 'two', label: 'Two'${toggleOptionsClassName ? ', classNames \'my-custom-class\'' : ''} },
+  {
+    type: 'group', name: 'group1', items: [
+      { value: 'three', label: 'Three' },
+      { value: 'four', label: 'Four'${toggleOptionsClassName ? ', className: \'my-custom-class\'' : ''} }
+    ]
+  },
+  {
+    type: 'group', name: 'group2', items: [
+      { value: 'five', label: 'Five' },
+      { value: 'six', label: 'Six' }
+    ]
+  }
 ]
-              `}
+`}
+            </pre>
+          </div>
+          <h4>Usage with custom classeNames: </h4>
+          <div className='code'>
+            <pre>{
+`
+<Dropdown
+  options={options}
+  value={defaultOption}
+  placeholder="Select an option"
+  className=${ toggleClassName ? '"my-custom-class"' : '""' }
+  placeholderClassName=${ togglePlaholderClassName ? '"my-custom-class"' : '""' }
+  menuClassName=${ toggleMenuClassName ? '"my-custom-class"' : '""' }
+/>
+`}
             </pre>
           </div>
         </section>
