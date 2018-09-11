04d5fae50cdcbb667c2abe5f4edf4120bb4c669e
Evgenii Sidorov <evgenii.sidorov4@gmail.com>
2018-09-11 18:03:35 +0300

    Added arrowClosed and arrowOpen to typing.

--- index.d.ts
+++ index.d.ts
@@ -19,6 +19,8 @@ declare module "react-dropdown" {
     menuClassName?: string;
     arrowClassName?: string;
     disabled?: boolean;
+    arrowClosed?: JSX.Element,
+    arrowOpen?: JSX.Element,
     onChange?: (arg: Option) => void;
     onFocus?: (arg: boolean) => void;
     value?: Option | string;
